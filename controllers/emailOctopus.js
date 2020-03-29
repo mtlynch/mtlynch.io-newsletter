41247cd5931e5e8a78c9870253793d20f2c445af
Michael Lynch <git@mtlynch.io>
2020-03-29 11:25:19 -0400

    Adding support for subscriptions endpoint

--- controllers/emailOctopus.js
+++ controllers/emailOctopus.js
@@ -1,4 +1,8 @@
+const http = require("http");
 const https = require("https");
+
+const API_HOSTNAME = "emailoctopus.com";
+const API_PORT = 443;
 
 // Validates a Email Octopus User ID like "7d763008-6c82-11ea-a3d0-06b4694bee2a".
 function validateUserId(userId) {
@@ -19,8 +23,8 @@ function updateContact(userId, data) {
 
     const listId = process.env.EMAIL_OCTOPUS_LIST_ID;
     const options = {
-      hostname: "emailoctopus.com",
-      port: 443,
+      hostname: API_HOSTNAME,
+      port: API_PORT,
       path: `/api/1.5/lists/${listId}/contacts/${userId}`,
       method: "PUT",
       headers: {
@@ -74,6 +78,59 @@ function updateUserTopic(userId, topic) {
   });
 }
 
+function subscribeUser(email, topic) {
+  return new Promise((resolve, reject) => {
+    if (!topic) {
+      reject(new Error("topic must be set"));
+      return;
+    }
+
+    const data = JSON.stringify({
+      api_key: process.env.EMAIL_OCTOPUS_API_KEY,
+      email_address: email,
+      fields: {
+        Topics: topic
+      }
+    });
+
+    const listId = process.env.EMAIL_OCTOPUS_LIST_ID;
+    const options = {
+      hostname: API_HOSTNAME,
+      port: API_PORT,
+      path: `/api/1.5/lists/${listId}/contacts`,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Content-Length": data.length
+      }
+    };
+
+    const req = https.request(options, res => {
+      if (res.statusCode == 409) {
+        console.log(`duplicate signup from ${email}`);
+      } else if (res.statusCode < 200 || res.statusCode > 299) {
+        reject(
+          new Error(
+            "Failed to complete request - " +
+              (res.statusMessage ? res.statusMessage : res.statusCode)
+          )
+        );
+      }
+
+      const body = [];
+      res.on("data", d => body.push(d));
+      res.on("end", () => resolve(body.join("")));
+    });
+
+    req.on("error", err => {
+      reject(err);
+    });
+
+    req.write(data);
+    req.end();
+  });
+}
+
 // Sets the user with the given ID to unsubscribed.
 function unsubscribeUser(userId) {
   return new Promise((resolve, reject) => {
@@ -89,6 +146,7 @@ function unsubscribeUser(userId) {
 }
 
 module.exports = {
+  subscribeUser,
   unsubscribeUser,
   updateUserTopic
 };
